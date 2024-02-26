1cae8930255a83b5476bed1e03fd14ddae9b6d96
allan b <https://github.com/allankobejordan/udemy_react.git>
2024-02-26 20:46:19 -0300

    7: Projeto 1 - Aprendendo conceito de Props

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,17 @@
 
 import './App.css'
 import Header from './components/Header/Header';
+import FinanceControl from './components/FinanceControl/FinanceControl';
 function App() {
+
+  const finances = {
+    id:1,
+    description:"expenses",
+  }
   return (
    <div>
     <Header/>
+    <FinanceControl/>
    </div>
 
   )
