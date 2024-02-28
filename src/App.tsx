a802dcbbe6b9559658b45e1e0d42a5cd534e8839
allan b <https://github.com/allankobejordan/udemy_react.git>
2024-02-28 20:54:05 -0300

    aula sobre useState e hooks

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,39 @@
 import './App.css'
 import Header from './components/Header/Header';
 import FinanceControl from './components/FinanceControl/FinanceControl';
-function App() {
+import { useState } from 'react';
+import { Movement } from './Models/interfaces/Movement/Movement';
 
-  const finances = {
-    id:1,
-    description:"expenses",
+function App() {
+  const [currentBalance, setCurrentBalance] = useState(0); //state de saldo atual 
+  const [currentExpenses, setCurrentExpenses] = useState(0); //state de despesas atual
+  const [movementsItems, setMovementsItems] = useState<Array<Movement>>([]); //state de movimentações
+
+  const setNewMovement = (movement: Movement) =>{
+    if(movement){
+      setMovementsItems((prevMovements)=>{
+        const movements = [...prevMovements];
+        movements.unshift({
+          name: movement.name,
+          value:movement.value,
+          type:movement.type,
+          id:Math.random().toString()
+        });
+        return movements;
+      });
+      movement.type ==="Input" && setCurrentBalance((prevBalance) => prevBalance + Number(movement.value));
+
+
+      movement.type ==="Output" && setCurrentExpenses((prevExpenses) => prevExpenses + Number(movement.value));
+
+      currentBalance > 0 && setCurrentBalance ((prevBalance)=> prevBalance - Number(movement.value));
+    }
   }
+
   return (
    <div>
-    <Header/>
-    <FinanceControl/>
+      <Header/>
+      <FinanceControl/>
    </div>
 
   )
